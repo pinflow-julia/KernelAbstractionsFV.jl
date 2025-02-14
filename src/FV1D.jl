a5b098e87cbf8c036f389be985900caae4d5da17
Arpit Babbar <arpitbabbar@gmail.com>
2025-02-14 11:33:17 +0100

    Add initial condition as a Kernel

--- src/FV1D.jl
+++ src/FV1D.jl
@@ -121,6 +121,12 @@ end
                                            initial_value, t)
     i = @index(Global, Linear)
     u[:, i] .= initial_value(xc[i], t, equations)
+@kernel function set_initial_value_kernel!(cache, grid, equations::AbstractEquations{1},
+                                           initial_value)
+    i = @index(Global, Linear)
+    (; u) = cache
+    (; nx, xc) = grid
+    u[:,i] .= initial_value(xc[i], 0.0, equations)
 end
 
 """
