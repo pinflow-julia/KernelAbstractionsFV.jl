a0fb86386acb1b38548c2efb1cbd23fc7d7f55cb
Arpit Babbar <arpitbabbar@gmail.com>
2025-02-15 23:36:40 +0100

    Add tests for metal GPU

---
+++ test/runtests_metal.jl
@@ -0,0 +1,27 @@
+using KernelAbstractionsFV
+using KernelAbstractionsFV: examples_dir
+using TrixiBase
+using Test
+using Metal
+
+@testset "Density wave CPU tests" begin
+    tspan = (0.0f0, 0.001f0)
+    nx = 10
+    trixi_include(joinpath(examples_dir, "run_dwave.jl"), tspan = tspan, nx = nx,
+                  backend_kernel = MetalBackend())
+    @test isapprox(sol.l1, 0.0002595529f0, atol = 1e-9, rtol = 1e-9)
+    @test isapprox(sol.l2, 0.00025771547f0, atol = 1e-9, rtol = 1e-9)
+    @test isapprox(sol.linf, 0.00037491322f0, atol = 1e-9, rtol = 1e-9)
+
+    trixi_include(joinpath(examples_dir, "elixir_dwave.jl"), tspan = tspan, nx = nx,
+                  backend_kernel = MetalBackend())
+    @test isapprox(sol.l1, 0.0002595529f0, atol = 1e-9, rtol = 1e-9)
+    @test isapprox(sol.l2, 0.00025771547f0, atol = 1e-9, rtol = 1e-9)
+    @test isapprox(sol.linf, 0.00037491322f0, atol = 1e-9, rtol = 1e-9)
+
+    trixi_include(joinpath(examples_dir, "cpu_dwave.jl"), tspan = tspan, nx = nx,
+                  backend_kernel = MetalBackend())
+    @test isapprox(sol.l1, 0.0002595529f0, atol = 1e-9, rtol = 1e-9)
+    @test isapprox(sol.l2, 0.00025771547f0, atol = 1e-9, rtol = 1e-9)
+    @test isapprox(sol.linf, 0.00037491322f0, atol = 1e-9, rtol = 1e-9)
+end
