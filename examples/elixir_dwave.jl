ef20c23db8f64c17938b50610f99cf2958442136
Arpit Babbar <arpitbabbar@gmail.com>
2025-02-15 20:32:25 +0100

    Improve examples

--- examples/elixir_dwave.jl
+++ examples/elixir_dwave.jl
@@ -1,26 +1,32 @@
 using KernelAbstractionsFV
 using Trixi
+using KernelAbstractions
 
-domain = (0.0, 1.0)
-nx = 800
-grid = make_grid(domain, nx)
-equations = CompressibleEulerEquations1D(1.4)
+RealT = Float32
+domain = map(RealT, (0.0, 1.0))
+nx = 400
+backend_kernel = CPU()
 
-function initial_condition_dwave(x, t, equations)
-    rho = 2.0 + 0.2*sin(2*pi*(x - 0.1*t))
-    v1 = 0.1
-    p = 1.0
+grid = make_grid(domain, nx, backend_kernel);
+equations = CompressibleEulerEquations1D(1.4f0)
+
+function initial_condition_dwave(x, t, equations::CompressibleEulerEquations1D)
+    RealT = eltype(x)
+    rho = map(RealT, 2.0f0 + 0.2f0*sin(2.0f0*pi*(x - 0.1f0*t)))
+    v1 = map(RealT, 0.1f0)
+    p = map(RealT, 1.0f0)
     return prim2cons((rho, v1, p), equations)
 end
-
 surface_flux = flux_rusanov
-semi = SemiDiscretizationHyperbolic(grid, equations, surface_flux, initial_condition_dwave)
-tspan = (0.0, 0.1)
+semi = SemiDiscretizationHyperbolic(grid, equations, surface_flux, initial_condition_dwave,
+                                    backend_kernel = backend_kernel
+                                    );
+tspan = map(RealT, (0.0, 0.1))
 ode = ODE(semi, tspan)
-Ccfl = 0.9
-save_time_interval = 0.0
+Ccfl = map(RealT, 0.9)
+save_time_interval = map(RealT, 0.0)
 param = Parameters(Ccfl, save_time_interval)
 
-sol = solve(ode, param)
+sol = solve(ode, param);
 
 @show sol.l1, sol.l2
