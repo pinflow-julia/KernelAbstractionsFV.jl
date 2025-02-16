3b4ec6f97fff797ddb00649df398e308de8b1925
Arpit Babbar <arpitbabbar@gmail.com>
2025-02-16 12:11:01 +0100

    Rename files, add metal example

--- test/runtests_metal.jl
+++ test/runtests_metal.jl
@@ -13,13 +13,18 @@ using Metal
     @test isapprox(sol.l2, 0.00025771547f0, atol = 1e-9, rtol = 1e-9)
     @test isapprox(sol.linf, 0.00037491322f0, atol = 1e-9, rtol = 1e-9)
 
+    trixi_include(joinpath(examples_dir, "run_metal_dwave.jl"), tspan = tspan, nx = nx)
+    @test isapprox(sol.l1, 0.0002595529f0, atol = 1e-9, rtol = 1e-9)
+    @test isapprox(sol.l2, 0.00025771547f0, atol = 1e-9, rtol = 1e-9)
+    @test isapprox(sol.linf, 0.00037491322f0, atol = 1e-9, rtol = 1e-9)
+
     trixi_include(joinpath(examples_dir, "elixir_dwave.jl"), tspan = tspan, nx = nx,
                   backend_kernel = MetalBackend())
     @test isapprox(sol.l1, 0.0002595529f0, atol = 1e-9, rtol = 1e-9)
     @test isapprox(sol.l2, 0.00025771547f0, atol = 1e-9, rtol = 1e-9)
     @test isapprox(sol.linf, 0.00037491322f0, atol = 1e-9, rtol = 1e-9)
 
-    trixi_include(joinpath(examples_dir, "cpu_dwave.jl"), tspan = tspan, nx = nx,
+    trixi_include(joinpath(examples_dir, "run_cpu_dwave.jl"), tspan = tspan, nx = nx,
                   backend_kernel = MetalBackend())
     @test isapprox(sol.l1, 0.0002595529f0, atol = 1e-9, rtol = 1e-9)
     @test isapprox(sol.l2, 0.00025771547f0, atol = 1e-9, rtol = 1e-9)
